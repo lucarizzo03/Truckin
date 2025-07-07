1c29480cde1b23b27148d6d4d4ed94c250aee67c
lucar <lucarizz@umich.edu>
2025-07-07 17:48:54 -0400

    commit

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,8 +1,9 @@
-const { getDefaultConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
 // Add support for expo-dev-client
 config.resolver.resolverMainFields = ['react-native', 'browser', 'main'];
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
 
 module.exports = config;
